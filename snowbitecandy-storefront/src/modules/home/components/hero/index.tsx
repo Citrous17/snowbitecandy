dc9ade596b1e1a49ba5166eb0a0506ed0f870d7d
Kyle Stallings <citrous@tamu.edu>
2025-04-19 22:05:33 -0500

    Initial Commit

---
+++ snowbitecandy-storefront/src/modules/home/components/hero/index.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { Github } from "@medusajs/icons"
+import { Button, Heading } from "@medusajs/ui"
+import { useRouter } from "next/navigation"
+
+const Hero = () => {
+  const router = useRouter()
+
+  const storeLink = `${process.env.NEXT_PUBLIC_BASE_URL}/categories/candy`
+  return (
+    <div className="h-[92vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+      <img
+        src="/HeroBackdrop.png"
+        alt="Background"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+        <span>
+        <div className="bg-white rounded-lg p-6 shadow-lg">
+          <Heading
+            level="h1"
+            className="text-3xl leading-10 text-white text-ui-fg-base font-normal"
+          >
+            Snow Bite Candy Store
+          </Heading>
+        </div>
+        </span>
+        <a
+          href={`${storeLink}`}
+          target="_blank"
+        >
+          <Button variant="secondary">
+            Order Now
+            <Github />
+          </Button>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ snowbitecandy/src/admin/vite.config.js
@@ -0,0 +1,5 @@
+export default {
+  server: {
+    allowedHosts: ['snowbitecandy.com', 'localhost']
+  }
+}
